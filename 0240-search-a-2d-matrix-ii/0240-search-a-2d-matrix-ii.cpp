0956efc704e87ef940833f50c04c008fae497a26
Parth Ingle <parthingle826@gmail.com>
2026-07-29 15:31:06 +0530

    Time: 65 ms (19.01%), Space: 18.8 MB (39.42%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int tar) {
+        int m=mat.size(),n=mat[0].size();
+        int r=0,c=n-1;
+        while(r<m && c>=0)
+        {
+            if(tar==mat[r][c])
+            {
+                return true;
+            }
+            else if(tar<=mat[r][c])
+            {
+                c--;
+            }
+            else
+            {
+                r++;
+            }
+        }
+        return false;
+    }
+};
