f3e32c4724fea432af5953f4d2e461410ab82b69
Parth Ingle <parthingle826@gmail.com>
2026-08-05 22:53:08 +0530

    Time: 22 ms (13.53%), Space: 31.6 MB (37.65%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int expSum=0,actSum=0;
-        unordered_set<int> s;
-        int a,b;
-        vector<int> ans;
-        for(int i=0;i<n;i++)
+       int n=grid.size();
+       int a,b,expSum=0,actSum=0;
+       vector<int> ans;
+       unordered_set<int>s;
+       for(int i=0;i<n;i++)
+       {
+        for(int j=0;j<n;j++)
         {
-            for (int j=0;j<n;j++)
+            actSum+=grid[i][j];
+            if(s.find(grid[i][j])!=s.end())
             {
-                actSum+=grid[i][j];
-                if(s.find(grid[i][j])!=s.end())
-                {
-                    a=grid[i][j];
-                    ans.push_back(a);
-                }
-                s.insert(grid[i][j]);
+                a=grid[i][j];
+                ans.push_back(a);
             }
-
+            s.insert(grid[i][j]);
         }
-        expSum=(n*n)*(n*n+1)/2;
-        b=expSum+a-actSum;
-        ans.push_back(b);
-        return ans;
+       }
+       expSum=(n*n)*(n*n+1)/2;
+       b=expSum+a-actSum;
+       ans.push_back(b);
+       return ans;
     }
 };
